cca309ef5e04575a7c46f92b3d608fa7bc17c383
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2024-09-21 17:03:49 +0600

    Initialize an app

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Campers Shop App");
+});
+
+export default app;
